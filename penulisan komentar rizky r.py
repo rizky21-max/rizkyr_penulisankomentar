fb4b4bf0f0a15ef52eafec9b3fd14d9f51ea5f26
rizky21-max <riskiivansyah51@gmail.com>
2024-09-27 10:24:22 +0700

    Add files via upload

---
+++ penulisan komentar rizky r.py
@@ -0,0 +1,10 @@
+# variabel a merepresentasikan panjang
+a = 5
+b = 10 # variabel b merepresentasikan tinggi
+
+"""
+dan variabel c merepresentasikan luas
+persegi dari hasil perkalian
+variabel a dan variabel b
+"""
+c = a * b
